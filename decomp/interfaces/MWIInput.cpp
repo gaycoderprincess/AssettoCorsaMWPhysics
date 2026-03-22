b1c8d5bda08e773d072c64f025fa71d2fef1a8a4
💖 Chloe 💖 <gaycoderprincess@proton.me>
2026-03-22 18:14:27 +0100

    cleanup input polling

--- decomp/interfaces/MWIInput.cpp
+++ decomp/interfaces/MWIInput.cpp
@@ -6,26 +6,49 @@ public:
 
 	Car* pCar;
 
+	virtual bool ShouldUseOriginalControls() {
+		if (!strcmp(pCar->controlsProvider->getName(), "Kunos Simulazioni Keyboard controls provider")) return false;
+		if (!strcmp(pCar->controlsProvider->getName(), "Kunos Simulazioni Joypad controls provider")) return false;
+		return true;
+	}
+
+	float GetOriginalControlGas() {
+		return pCar->controls.gas;
+	}
+	float GetOriginalControlBrake() {
+		return pCar->controls.brake;
+	}
+	float GetOriginalControlHandBrake() {
+		return pCar->controls.handBrake;
+	}
+	float GetOriginalControlSteering() {
+		return -pCar->controls.steer;
+	}
+
 	virtual bool AreControlsBlocked() {
 		return pCar->isControlsLocked || pCar->lockControlsTime > pCar->ksPhysics->physicsTime;
 	}
 	virtual float GetControlGas() {
 		if (AreControlsBlocked()) return 0.0;
+		if (ShouldPlayerUseOriginalControls()) return GetOriginalControlGas();
 		if (IsKeyPressed(VK_UP)) return 1.0;
 		return GetPadKeyState(NYA_PAD_KEY_RT, -1) / 255.0;
 	}
 	virtual float GetControlBrake() {
 		if (AreControlsBlocked()) return 0.0;
+		if (ShouldPlayerUseOriginalControls()) return GetOriginalControlBrake();
 		if (IsKeyPressed(VK_DOWN)) return 1.0;
 		return GetPadKeyState(NYA_PAD_KEY_LT, -1) / 255.0;
 	}
 	virtual float GetControlHandBrake() {
 		if (AreControlsBlocked()) return 1.0;
+		if (ShouldPlayerUseOriginalControls()) return GetOriginalControlHandBrake();
 		if (IsKeyPressed(VK_SPACE) || IsPadKeyPressed(NYA_PAD_KEY_A, -1)) return 1.0;
 		return 0.0;
 	}
 	virtual float GetControlSteering() {
 		if (AreControlsBlocked()) return 0.0;
+		if (ShouldPlayerUseOriginalControls()) return GetOriginalControlSteering();
 
 		float f = 0;
 		if (IsKeyPressed(VK_LEFT)) f += 1.0;
@@ -38,10 +61,6 @@ public:
 		if (AreControlsBlocked()) return false;
 		return IsKeyPressed(VK_MENU) || IsPadKeyPressed(NYA_PAD_KEY_B, -1);
 	}
-	//virtual float GetControlGas() { return pCar->controls.gas; }
-	//virtual float GetControlBrake() { return pCar->controls.brake; }
-	//virtual float GetControlHandBrake() { return pCar->controls.handBrake; }
-	//virtual float GetControlSteering() { return -pCar->controls.steer; }
 	virtual bool IsAutomaticShift() { return pCar->autoShift.isActive; }
 };
 
@@ -49,26 +68,13 @@ class IInputAI : public IInput {
 public:
 	IInputAI(Car* car) : IInput(car) {}
 
-	virtual float GetControlGas() {
-		if (AreControlsBlocked()) return 0.0;
-		return pCar->controls.gas;
-	}
-	virtual float GetControlBrake() {
-		if (AreControlsBlocked()) return 0.0;
-		return pCar->controls.brake;
-	}
-	virtual float GetControlHandBrake() {
-		if (AreControlsBlocked()) return 1.0;
-		return pCar->controls.handBrake;
-	}
-	virtual float GetControlSteering() {
-		if (AreControlsBlocked()) return 0.0;
-		return -pCar->controls.steer;
+	virtual bool ShouldUseOriginalControls() {
+		return true;
 	}
 	virtual bool GetControlNOS() {
 		if (!bNitrousEnabled) return false;
 		if (AreControlsBlocked()) return false;
-		return false;
+		return GetControlGas() >= 0.99; // automatically nos when full throttle
 	}
 	virtual bool IsAutomaticShift() { return true; } // ai uses automatic shift
 };
