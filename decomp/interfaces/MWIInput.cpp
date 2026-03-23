f85e2b2c132874ad8daecf4d5a5868eadc84ca0a
💖 Chloe 💖 <gaycoderprincess@proton.me>
2026-03-23 16:10:07 +0100

    update readme, use vanilla keyboard bindings

--- decomp/interfaces/MWIInput.cpp
+++ decomp/interfaces/MWIInput.cpp
@@ -6,10 +6,16 @@ public:
 
 	Car* pCar;
 
+private:
 	virtual bool ShouldUseOriginalControls() {
 		if (!strcmp(pCar->controlsProvider->getName(), "Kunos Simulazioni Keyboard controls provider")) return false;
 		if (!strcmp(pCar->controlsProvider->getName(), "Kunos Simulazioni Joypad controls provider")) return false;
 		return true;
+	}
+
+	KeyboardCarControl* GetKeyboardController() {
+		if (!strcmp(pCar->controlsProvider->getName(), "Kunos Simulazioni Keyboard controls provider")) return (KeyboardCarControl*)pCar->controlsProvider;
+		return nullptr;
 	}
 
 	float GetOriginalControlGas() {
@@ -25,34 +31,45 @@ public:
 		return -pCar->controls.steer;
 	}
 
+public:
 	virtual bool AreControlsBlocked() {
 		return pCar->isControlsLocked || pCar->lockControlsTime > pCar->ksPhysics->physicsTime;
 	}
 	virtual float GetControlGas() {
 		if (AreControlsBlocked()) return 0.0;
 		if (ShouldUseOriginalControls()) return GetOriginalControlGas();
-		if (IsKeyPressed(VK_UP)) return 1.0;
+		auto keyboard = GetKeyboardController();
+		if (IsKeyPressed(keyboard ? keyboard->keyGas : VK_UP)) return 1.0;
 		return GetPadKeyState(NYA_PAD_KEY_RT, -1) / 255.0;
 	}
 	virtual float GetControlBrake() {
 		if (AreControlsBlocked()) return 0.0;
 		if (ShouldUseOriginalControls()) return GetOriginalControlBrake();
-		if (IsKeyPressed(VK_DOWN)) return 1.0;
+		auto keyboard = GetKeyboardController();
+		if (IsKeyPressed(keyboard ? keyboard->keyBrake : VK_DOWN)) return 1.0;
 		return GetPadKeyState(NYA_PAD_KEY_LT, -1) / 255.0;
 	}
 	virtual float GetControlHandBrake() {
 		if (AreControlsBlocked()) return 1.0;
 		if (ShouldUseOriginalControls()) return GetOriginalControlHandBrake();
-		if (IsKeyPressed(VK_SPACE) || IsPadKeyPressed(NYA_PAD_KEY_A, -1)) return 1.0;
+		auto keyboard = GetKeyboardController();
+		if (IsKeyPressed(keyboard ? keyboard->keyHandbrake : VK_SPACE) || IsPadKeyPressed(NYA_PAD_KEY_A, -1)) return 1.0;
 		return 0.0;
 	}
 	virtual float GetControlSteering() {
 		if (AreControlsBlocked()) return 0.0;
 		if (ShouldUseOriginalControls()) return GetOriginalControlSteering();
 
+		int keyL = VK_LEFT;
+		int keyR = VK_RIGHT;
+		if (auto keyboard = GetKeyboardController()) {
+			keyL = keyboard->keyLeft;
+			keyR = keyboard->keyRight;
+		}
+
 		float f = 0;
-		if (IsKeyPressed(VK_LEFT)) f += 1.0;
-		if (IsKeyPressed(VK_RIGHT)) f -= 1.0;
+		if (IsKeyPressed(keyL)) f += 1.0;
+		if (IsKeyPressed(keyR)) f -= 1.0;
 		f -= GetPadKeyState(NYA_PAD_KEY_LSTICK_X, -1) / 32768.0;
 		return f;
 	}
@@ -68,9 +85,11 @@ class IInputAI : public IInput {
 public:
 	IInputAI(Car* car) : IInput(car) {}
 
+private:
 	virtual bool ShouldUseOriginalControls() {
 		return true;
 	}
+public:
 	virtual bool GetControlNOS() {
 		if (!bNitrousEnabled) return false;
 		if (AreControlsBlocked()) return false;
